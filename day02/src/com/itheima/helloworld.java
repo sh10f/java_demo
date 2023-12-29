ad6e45f18398b5e0f9cf8ea74651c987fffc2476
Fsh <fushihaocqu@163.com>
2023-12-29 20:14:24 +0800

    初始化项目

---
+++ day02/src/com/itheima/helloworld.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+import java.util.Scanner;
+
+public class helloworld {
+    public static void main(String[] args) {
+        // line in x
+        System.out.println("Hello, world!");
+
+        Scanner
+    }
+}
---
+++ src/bit_divide.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class bit_divide {
+    public static void main(String[] args) {
+
+
+        Random r = new Random();
+        int num = r.nextInt(100) + 1;
+        Scanner sc = new Scanner(System.in);
+        int x;
+
+        do{
+            x = sc.nextInt();
+            if(x > num)
+                System.out.println("猜大了");
+            else if(x < num)
+                System.out.println("猜小了");
+            else
+                System.out.println("good");
+        }while (x != num);
+    }
+
+}
+
